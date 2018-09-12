9df08b439606865d58e61175ca580a78f8a4b54c
Aykut Yaman <yaman.ayk@gmail.com>
2018-09-12 14:05:50 +0300

    mergeRanges function implemented

--- interview-cake/merging-meeting-times/mergeRanges.test.ts
+++ interview-cake/merging-meeting-times/mergeRanges.test.ts
@@ -33,3 +33,41 @@ test('multiple merged meetings', () => {
   let expected = [{ startTime: 1, endTime: 8 }];
   expect(actual).toEqual(expected);
 });
+
+test('meetings not sorted', () => {
+  let actual = mergeRanges([
+    { startTime: 5, endTime: 8 },
+    { startTime: 1, endTime: 4 },
+    { startTime: 6, endTime: 8 },
+  ]);
+  let expected = [{ startTime: 1, endTime: 4 }, { startTime: 5, endTime: 8 }];
+  expect(actual).toEqual(expected);
+});
+
+test('one long meeting containing smaller meetings', () => {
+  const actual = mergeRanges([
+    { startTime: 1, endTime: 10 },
+    { startTime: 2, endTime: 5 },
+    { startTime: 6, endTime: 8 },
+    { startTime: 9, endTime: 10 },
+    { startTime: 10, endTime: 12 }
+  ]);
+  const expected = [{ startTime: 1, endTime: 12 }];
+  expect(actual).toEqual(expected);
+});
+
+test('sample input', () => {
+  const actual = mergeRanges([
+    { startTime: 0, endTime: 1 },
+    { startTime: 3, endTime: 5 },
+    { startTime: 4, endTime: 8 },
+    { startTime: 10, endTime: 12 },
+    { startTime: 9, endTime: 10 },
+  ]);
+  const expected = [
+    { startTime: 0, endTime: 1 },
+    { startTime: 3, endTime: 8 },
+    { startTime: 9, endTime: 12 },
+  ];
+  expect(actual).toEqual(expected);
+});
---
+++ interview-cake/merging-meeting-times/mergeRanges.ts
@@ -0,0 +1,21 @@
+const mergeRanges = ranges => {
+  const sorted = [...ranges].sort((x, y) => x.startTime - y.startTime);
+  const merged = [sorted[0]];
+
+  for (let i = 1; i < sorted.length; i++) {
+    let current = sorted[i];
+    let lastMerged = merged[merged.length - 1];
+
+    if (current.startTime <= lastMerged.endTime) { // merge
+      lastMerged.endTime = Math.max(current.endTime, lastMerged.endTime);
+    } else {
+      merged.push(current);
+    }
+  }
+
+  return merged;
+};
+
+export {
+  mergeRanges
+}
