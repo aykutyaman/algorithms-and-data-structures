4a277f89c6f02eda772022ee13f75c78b8155e43
Aykut Yaman <yaman.ayk@gmail.com>
2018-09-21 07:57:09 +0300

    heap

--- data-structures/heap/heap.test.ts
+++ data-structures/heap/heap.test.ts
@@ -1,6 +1,9 @@
 import {
   heap,
-  add
+  add,
+  isEmpty,
+  peek,
+  extractRoot
 } from './heap';
 
 test('add', () => {
@@ -15,4 +18,69 @@ test('moves new node, to a proper place', () => {
   const hp = heap([71, 19, 35, 12, 4], compare);
   add(hp, 43);
   expect(hp.data).toEqual([71, 19, 43, 12, 4, 35]);
+});
+
+test('heapify up to the root', () => {
+  const compare = (x, y) => (x - y);
+  const hp = heap([44, 42, 35, 33, 31, 19, 27], compare);
+  add(hp, 50);
+  expect(hp.data).toEqual([50, 44, 35, 42, 31, 19, 27, 33]);
+});
+
+test('add items to the heap, and heapify up and down', () => {
+  const compare = (x, y) => (x - y);
+  const hp = heap([], compare);
+
+  add(hp, 5);
+  expect(isEmpty(hp)).toBe(false);
+  expect(peek(hp)).toBe(5);
+  expect(hp.data).toEqual([5]);
+
+  add(hp, 3);
+  expect(peek(hp)).toBe(5);
+  expect(hp.data).toEqual([5, 3]);
+
+  add(hp, 10);
+  expect(peek(hp)).toBe(10);
+  expect(hp.data).toEqual([10, 3, 5]);
+
+  add(hp, 1);
+  expect(peek(hp)).toBe(10);
+  expect(hp.data).toEqual([10, 3, 5, 1]);
+
+  add(hp, 1);
+  expect(peek(hp)).toBe(10);
+  expect(hp.data).toEqual([10, 3, 5, 1, 1]);
+
+  expect(extractRoot(hp)).toBe(10);
+  expect(hp.data).toEqual([5, 3, 1, 1]);
+
+  expect(extractRoot(hp)).toBe(5);
+  expect(hp.data).toEqual([3, 1, 1]);
+
+  expect(extractRoot(hp)).toBe(3);
+  expect(hp.data).toEqual([1, 1]);
+
+  expect(extractRoot(hp)).toBe(1);
+  expect(hp.data).toEqual([1]);
+
+  expect(extractRoot(hp)).toBe(1);
+  expect(hp.data).toEqual([]);
+});
+
+test('heapify down through the right branch', () => {
+  const compare = (x, y) => (x - y);
+  const hp = heap([], compare);
+
+  add(hp, 3);
+  add(hp, 12);
+  add(hp, 10);
+
+  expect(hp.data).toEqual([12, 3, 10]);
+
+  add(hp, 11);
+  expect(hp.data).toEqual([12, 11, 10, 3]);
+
+  expect(extractRoot(hp)).toBe(12);
+  expect(hp.data).toEqual([11, 3, 10]);
 });
--- data-structures/heap/heap.ts
+++ data-structures/heap/heap.ts
@@ -11,7 +11,7 @@ const heap = (data: any[], compare: Function): Heap => ({
 const parentIndex = (index: number) => (
   index === 0
     ? index
-    : Math.floor(index - 1) / 2
+    : Math.floor((index - 1) / 2)
 )
 
 const hasParent = (childIndex: number) => (
@@ -21,20 +21,67 @@ const hasParent = (childIndex: number) => (
 const notInCorrectOrder = (current, parent, heap: Heap) => (
   heap.compare(heap.data[parent], heap.data[current]) < 0
 )
-
+const inCorrectOrder = (current, child, heap: Heap) => (
+  heap.compare(heap.data[current], heap.data[child]) > 0
+)
 const swap = (current: number, parent: number, heap: Heap): void => {
   [heap.data[parent], heap.data[current]] = [heap.data[current], heap.data[parent]];
 }
 
 const heapifyUp = (current: number, heap: Heap): void => {
-  while (hasParent(current) && notInCorrectOrder(current, parentIndex(current), heap)) {
+  while (
+    hasParent(current) && notInCorrectOrder(current, parentIndex(current), heap)
+  ) {
     swap(current, parentIndex(current), heap);
     current = parentIndex(current);
   }
 }
 
-const extractRoot = (heap: Heap) => {
+const leftChildIndex = (parent: number): number => (
+  (2 * parent) + 1
+)
 
+const hasLeftChild = (parent: number, heap: Heap): Boolean => (
+  leftChildIndex(parent) < heap.data.length - 1
+)
+const rightChildIndex = (parent: number): number => (
+  (2 * parent) + 2
+)
+const hasRightChild = (parent: number, heap: Heap): Boolean => (
+  rightChildIndex(parent) < heap.data.length
+)
+
+const heapifyDown = (current: number, heap: Heap): void => {
+  let nextIndex = null;
+  while (hasLeftChild(current, heap)) {
+    if (hasRightChild(current, heap)
+      && heap.compare(heap.data[leftChildIndex(current)], heap.data[rightChildIndex(current)]) > 0) {
+      nextIndex = leftChildIndex(current);
+    } else {
+      nextIndex = rightChildIndex(current);
+    }
+    if (inCorrectOrder(current, nextIndex, heap)) {
+      break;
+    }
+    swap(current, nextIndex, heap);
+    current = nextIndex;
+  }
+}
+
+const extractRoot = (heap: Heap): any => {
+  if (heap.data.length === 0) {
+    return null;
+  }
+  if (heap.data.length === 1) {
+    return heap.data.pop();
+  }
+  const item = heap.data[0];
+
+  // move the last element to head
+  heap.data[0] = heap.data.pop();
+  heapifyDown(0, heap);
+
+  return item;
 }
 
 const add = (heap: Heap, element: any): void => {
@@ -42,8 +89,20 @@ const add = (heap: Heap, element: any): void => {
   heapifyUp(heap.data.length - 1, heap);
 }
 
+const isEmpty = (heap: Heap): Boolean => (
+  heap.data.length === 0
+)
+
+const peek = (heap: Heap): number | null => (
+  heap.data.length === 0
+    ? null
+    : heap.data[0]
+)
+
 export {
   heap,
   add,
-  extractRoot
+  extractRoot,
+  isEmpty,
+  peek
 }
