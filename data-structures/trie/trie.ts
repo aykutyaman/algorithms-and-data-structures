eb440f6bed620d9a6c276bba8b25714ad7937faa
Aykut Yaman <yaman.ayk@gmail.com>
2018-09-24 05:50:28 +0300

    trie

--- data-structures/trie/trie.ts
+++ data-structures/trie/trie.ts
@@ -1,37 +1,53 @@
 interface ITrie {
-  letter: string,
   fullWord: string,
   children: Map<string, any> // TODO: What is proper type?
 }
 
-const Trie = (letter = '', fullWord = '', children = new Map()): ITrie => ({
-  letter,
+const Trie = (fullWord = '', children = new Map()): ITrie => ({
   fullWord,
   children
 })
 
-const dump = (trie: ITrie) => JSON.stringify(trie)
+const dump = (trie: ITrie) => JSON.parse(JSON.stringify(trie)) // hack:remove Map name
 
-const reducer = (acc: ITrie, char: string) => acc
+const fullWord = (i, array) => (
+  (i === array.length - 1)
+    ? array.join('')
+    : ''
+)
+
+const reducer = (
+  trie: ITrie, letter: string, i: number, array: string[]
+): ITrie => {
+  trie.children[letter] = trie.children[letter] || Trie()
+
+  const full = fullWord(i, array)
+  if (full !== '') {
+    trie.children[letter].fullWord = full;
+  }
+  return trie.children[letter];
+}
 
 const add = (word: string) => (trie: ITrie): ITrie => (
-  word.split('').reduce(reducer, trie)
+  word.split('').reduce(reducer, trie) && trie // ugly! cannot return reduce result
 )
 
-const getChild = (char: string) => (trie: ITrie): ITrie => (
-  trie.children.has(char)
-    ? trie.children.get(char)
-    : null
-)
+const has = (word: string) => (trie: ITrie): Boolean => {
+  let temp = trie
+  for (let i = 0; i < word.length; i++) {
+    if (!temp.children[word[i]]) {
+      return false
+    }
+    temp = temp.children[word[i]]
+  }
+  return (temp !== null) && (temp.fullWord === word)
+}
 
 export {
   Trie,
   dump,
   add,
-  getChild
+  has
 }
 
-
-
 // remove = (word: string) => (trie: ITrie): ITrie => ()
-// lookup = (word: string) => (trie: ITrie): Boolean => ()
