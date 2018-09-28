1f83501db7aea3f7f89f2f3f5fd89f7fb2a7c320
Aykut Yaman <yaman.ayk@gmail.com>
2018-09-28 16:33:15 +0300

    refactored single riffle shuffle

--- interview-cake/single-riffle-shuffle/isSingleRiffle.ts
+++ interview-cake/single-riffle-shuffle/isSingleRiffle.ts
@@ -1,23 +1,32 @@
-// https://bit.ly/2MschJH
+const isSingleRiffle = (
+  half1: number[], half2: number[], deck: number[]
+): Boolean => {
+  let first = 0;
+  let second = 0;
 
-const isSingleRiffle = (half1: number[], half2: number[], deck: number[]) => {
-  let half1Index = 0;
-  let half2Index = 0;
-  let half1MaxIndex = half1.length - 1;
-  let half2MaxIndex = half2.length - 1;
-
+  // iterate through shuffled deck
   for (let i = 0; i < deck.length; i++) {
-    if (half1Index <= half1MaxIndex && deck[i] === half1[half1Index]) {
-      half1Index++;
-    } else if (half2Index <= half2MaxIndex && deck[i] === half2[half2Index]) {
-      half2Index++;
-    } else {
+
+    if (half1[first] && half1[first] === deck[i]) { // from first half
+      first++;
+    } else if (half2[second] && half2[second] === deck[i]) { // from second half
+      second++;
+    } else { // short circuit
       return false;
     }
   }
-  return true;
+
+  // deck length must match with sum of first and second half counters
+  return (first + second) === deck.length;
 }
 
 export {
   isSingleRiffle
 }
+
+
+/**
+  Complexity
+  - O(n) time cost
+  - O(1) space cost
+*/
