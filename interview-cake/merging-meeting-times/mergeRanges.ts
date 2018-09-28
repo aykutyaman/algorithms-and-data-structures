10c6dcef1f1449a59c9403554a6c87e34ded7da8
Aykut Yaman <yaman.ayk@gmail.com>
2018-09-28 06:31:15 +0300

    refactoring merging meeting times

--- interview-cake/merging-meeting-times/mergeRanges.ts
+++ interview-cake/merging-meeting-times/mergeRanges.ts
@@ -1,26 +1,53 @@
-interface meeting {
+interface Range {
   startTime: number,
   endTime: number
 }
 
-const mergeRanges = (ranges: meeting[]) => {
-  const sorted = [...ranges].sort((x, y) => x.startTime - y.startTime);
-  const merged = [sorted[0]];
+const overlaps = (first: Range, second: Range): Boolean => (
+  first.endTime >= second.startTime
+)
 
-  for (let i = 1; i < sorted.length; i++) {
-    let current = sorted[i];
-    let lastMerged = merged[merged.length - 1];
+const selectEndTime = (first: Range, second: Range) => (
+  (first.endTime > second.endTime)
+    ? first.endTime
+    : second.endTime
+)
 
-    if (current.startTime <= lastMerged.endTime) { // merge
-      lastMerged.endTime = Math.max(current.endTime, lastMerged.endTime);
+const mergeRanges = (ranges: Range[]): Range[] => {
+  // sort ranges by start time
+  const sorted = ranges.sort((x, y) => x.startTime - y.startTime);
+
+  // create a new array for storing merged ranges
+  const merged = [];
+
+  // iterate through ranges
+  for (let i = 0; i < sorted.length; i++) {
+    let first = merged[merged.length - 1];
+    let second = sorted[i];
+
+    // if first range overlaps with the second range
+    if (first && overlaps(first, second)) {
+
+      // merge first range with second range
+      first.endTime = selectEndTime(first, second);
     } else {
-      merged.push(current);
+
+      // else push second range into merged ranges
+      merged.push(second);
     }
   }
 
   return merged;
-};
+}
 
 export {
   mergeRanges
 }
+
+/**
+ Complexity
+ O(n lg n) time and O(n) space
+ - We sort all meetings first, this gives us O(n lg n) time cost
+ - We create a new array of merged times. In the worst case, none of the meetings
+ overlap, giving an array identical to the input array. Thus O(n) space cost
+ */
