ca43493fc9fdc9d5350ac53665facdb33d4f4791
Aykut Yaman <yaman.ayk@gmail.com>
2018-12-04 21:01:46 +0300

    highestProduct

--- interview-cake/highest-product/highestProduct.test.ts
+++ interview-cake/highest-product/highestProduct.test.ts
@@ -1,6 +1,16 @@
 import { highestProduct } from './highestProduct';
 
-test('highestProduct', () => {
-  // expect(highestProduct([1, 2, 3, 4])).toBe(24);
-  // expect(highestProduct([-1, -3, -4, -99])).toBe(24);
+test('highestProduct = positive numbers', () => {
+  expect(highestProduct([2, 3, 4, 1])).toBe(24)
+  expect(highestProduct([1, 2, 3, 4])).toBe(24)
+  expect(highestProduct([6, 1, 3, 5, 7, 8, 2])).toBe(336)
+  expect(highestProduct([-5, 4, 8, 2, 3])).toBe(96)
+  expect(highestProduct([-10, 1, 3, 2, -10])).toBe(300)
+  expect(highestProduct([-5, -1, -3, -2])).toBe(-6)
 });
+
+test('error with empty array', () => {
+  expect(() => {
+    highestProduct([])
+  }).toThrow()
+})
--- interview-cake/highest-product/highestProduct.ts
+++ interview-cake/highest-product/highestProduct.ts
@@ -1,31 +1,45 @@
-const head = ([x]): number => x;
-const tail = ([, ...xs]) => xs;
+// TODO: ugly! refactor
 
+const greatestTreeProduct = (n: Array<number>, acc) => {
+  const first = (n[0] * n[1] * n[n.length - 1])
+  const last = (n[n.length - 3] * n[n.length - 2] * n[n.length - 1])
 
-// xs can have maximum 3 elements
-const highestReducer = (x, xs) => {
-  // return new xs
-  const copy = [...xs].sort((x, y) => y - x);
-  copy[2] = x > copy[2] ? x : copy[2];
-  return copy;
+  if (first > last) {
+    if (first > acc) return first
+  } else {
+    if (last > acc) return last
+  }
+  return acc
 }
 
-const highest3 = (arr, result) => (
-  arr.length === 0
-    ? result
-    : highest3(tail(arr), highestReducer(head(arr), result))
-)
+const highestProduct = (nums: Array<number>): number => {
+  if (nums.length < 3) throw Error('nums must has at least three numbers')
 
-const makeHighest = (arr) => ([arr[0], arr[1], arr[2]]);
-const highestProduct = (arr: [number]) => {
-  // find highest 3 numbers
-  return highest3(arr, makeHighest(arr));
+  const map = new Map([['numbers', []]])
 
-  // find lowest 2 numbers
+  return nums.reduce((acc, num) => {
 
-  // return highest product with highest 3 and lowest 2
+    const saved = map.get('numbers')
 
-  return 24;
+    // save 5 numbers to the  hash table
+    if (saved.length < 5) {
+      map.set('numbers', [...saved, num].sort((x, y) => x - y))
+      if (saved.length < 3) return acc
+    } else {
+      const tmp = [...saved]
+      if (num < saved[1]) {
+        tmp[1] = num
+      }
+      if (num > saved[saved.length - 3]) {
+        tmp[saved.length - 3] = num
+      }
+      map.set('numbers', tmp.sort((x, y) => x - y))
+    }
+
+    // find the greatest three numbers
+    return greatestTreeProduct(map.get('numbers'), acc)
+
+  }, nums[0] * nums[1] * nums[2])
 }
 
 export {
